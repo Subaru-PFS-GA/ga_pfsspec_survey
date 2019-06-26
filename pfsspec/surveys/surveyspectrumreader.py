e0290b841807fd8ddd48ffe4f96736a8a4d8d9a0
Laszlo Dobos <dobos@complex.elte.hu>
2019-06-26 16:49:32 -0400

    Improvements to SDSS catalog export.

--- pfsspec/surveys/surveyspectrumreader.py
+++ pfsspec/surveys/surveyspectrumreader.py
@@ -1,3 +1,5 @@
+import sys
+
 from pfsspec.io.spectrumreader import SpectrumReader
 from pfsspec.surveys.survey import Survey
 from pfsspec.parallel import prll_map
@@ -9,12 +11,19 @@ class SurveySpectrumReader(SpectrumReader):
     def load_spectrum(self, row):
         raise NotImplementedError()
 
+    def load_spectrum_wrapper(self, row):
+        try:
+            return self.load_spectrum(row)
+        except Exception as e:
+            print(e, file=sys.stderr)   # TODO
+            return None
+
     def load_survey(self, params):
         survey = Survey()
         survey.params = params
         survey.spectra = []
 
         rows = [rows for index, rows in params.iterrows()]
-        survey.spectra = prll_map(self.load_spectrum, rows, verbose=True)
+        survey.spectra = prll_map(self.load_spectrum_wrapper, rows, verbose=True)
 
         return survey
