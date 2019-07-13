e640a8a2c36fbeb52f8a58f3b30f33195e94b644
Laszlo Dobos <dobos@complex.elte.hu>
2019-07-12 22:58:59 -0400

    Added base class for scripts. Train on kurucz, still not nice. Have to update other scripts.

--- test/ml/dnn/keras/test_densegenerative.py
+++ test/ml/dnn/keras/test_densegenerative.py
@@ -29,3 +29,24 @@ class TestDenseGenerative(TestBase):
         model.load_weights(model.checkpoint_path)
         model.predict(validation_generator)
 
+    def test_train_nosplit(self):
+        dataset = Dataset()
+        dataset.load(os.path.join(self.PFSSPEC_DATA_PATH, 'pfs_spec_test', 'kurucz_test', 'dataset.dat'))
+        dataset.flux = dataset.flux[:1000]
+        dataset.params = dataset.params[:1000]
+        split_index, ts, vs = dataset.split(0.9)
+
+        labels = ['t_eff', 'fe_h', 'log_g', ]
+        coeffs = [1000, 1, 1]
+
+        generator = KuruczGenerativeAugmenter(ts, labels, coeffs, batch_size=200, shuffle=True)
+        training_generator = generator.copy()
+        validation_generator = generator.copy()
+
+        model = DenseGenerative(levels=1, units=32)
+        model.loss = max_absolute_error
+        model.epochs = 1
+        model.ensure_model_created(training_generator.input_shape, training_generator.output_shape)
+        model.train(training_generator, validation_generator)
+        model.load_weights(model.checkpoint_path)
+        model.predict(validation_generator)
