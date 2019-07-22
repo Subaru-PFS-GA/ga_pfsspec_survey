e6ca6ea767081fc068a6c1c46f065535d6c109be
Laszlo Dobos <dobos@complex.elte.hu>
2019-07-22 11:12:15 -0400

    Added realistic simulated noise and run evaluation notebooks after dataset generation.

---
+++ test/obsmod/test_noise.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from test.test_base import TestBase
+
+from pfsspec.obsmod.noise import Noise
+
+class TestNoise(TestBase):
+    def test_read(self):
+        filename = os.path.join(self.PFSSPEC_DATA_PATH, 'subaru/pfs/noise/sim8hr.dat')
+        noise = Noise()
+        noise.read(filename)
+        noise.plot()
+
+        self.assertEqual((12288,), noise.wave.shape)
+        self.assertEqual((12288,), noise.noise.shape)
+
+        self.save_fig()
+
+    def test_resample(self):
+        filename = os.path.join(self.PFSSPEC_DATA_PATH, 'subaru/pfs/noise/sim8hr.dat')
+        noise = Noise()
+        noise.read(filename)
+        noise.plot()
+
+        nwave = np.linspace(3810, 12590, 3600)
+        noise.resample(nwave)
+
+        self.assertEqual((3600,), noise.wave.shape)
+        self.assertEqual((3600,), noise.noise.shape)
+
+        self.save_fig()
